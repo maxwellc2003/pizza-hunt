60ea53b72557428aa2655a81345723ffd8f812d6
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2022-12-03 00:19:31 -0800

    finalized

--- models/pizza.js
+++ models/pizza.js
@@ -5,9 +5,13 @@ const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
       type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -16,7 +20,9 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
-      default: "Large",
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+      default: 'Large'
     },
     toppings: [],
     comments: [
@@ -36,8 +42,8 @@ const PizzaSchema = new Schema(
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual("commentCount").get(function () {
-  return this.comments.length;
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the Pizza model using the PizzaSchema
